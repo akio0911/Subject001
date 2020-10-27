db6d826d0a9f1a20d15ea42dc25e9e99b07e6ac5
Masaki <anthrgrnwrld@gmail.com>
2020-10-27 19:17:53 +0900

    課題1 回答 Rev.01

--- Subject001/ViewController.swift
+++ Subject001/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet var textNumberField: [UITextField]!
+	@IBOutlet weak var labelResult: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		//textNumberFieldをタップした時に表示するキーボードを数字のみに
+		for field in textNumberField {
+			field.keyboardType = UIKeyboardType.numberPad
+		}
 	}
 
-
+	//ボタンを押した時
+	@IBAction func pressButton(_ sender: Any) {
+		labelResult.text = "\(calcTotalofTextNumberField())"
+	}
+	
+	//textNumberFieldの合計を計算する
+	private func calcTotalofTextNumberField() -> Int {
+		let numberResult = textNumberField.map {
+				let numberText = $0.text ?? ""
+				return Int(numberText) ?? 0
+			}.reduce(0) {
+				$0 + $1
+			}
+		return numberResult
+	}
+	
+	
 }
 
